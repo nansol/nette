682f8551c429f7dd05c0ccdfcb3a85b1afdea216
nansol <nancysgithub@gmail.com>
2019-11-05 20:33:08 +0100

    Change Route Factory

--- nette-calculator/app/Router/RouterFactory.php
+++ nette-calculator/app/Router/RouterFactory.php
@@ -4,18 +4,20 @@ declare(strict_types=1);
 
 namespace App\Router;
 
-use Nette;
+use Nette\Application\IRouter;
+use Nette\Application\Routers\Route;
 use Nette\Application\Routers\RouteList;
+use Nette\StaticClass;
 
 
-final class RouterFactory
+class RouterFactory
 {
 	use Nette\StaticClass;
 
 	public static function createRouter(): RouteList
 	{
 		$router = new RouteList;
-		$router->addRoute('<presenter>/<action>[/<id>]', 'Homepage:default');
+		$router->addRoute('<presenter>/<action>[/<id>]', 'Calculator:default');
 		return $router;
 	}
 }
